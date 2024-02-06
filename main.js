c521995df26b0cf194a4d75819853fd60fd254eb
Gren4586 <142317951+Gren4586@users.noreply.github.com>
2024-02-06 10:53:39 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+let input = "Dude, we should go eat some plankton!";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === 'u'){
+    resultArray.push(input[i]);
+  }
+
+ for (let v = 0; v < vowels.length; v++) {
+  
+  if (input[i] === vowels[v]) {
+    
+    resultArray.push(input[i]);
+    
+  }
+ }
+}
+let resultString = resultArray.join('').toUpperCase();
+console.log(resultArray);
+console.log(resultString);
